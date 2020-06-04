3ab9763e8a52735396d7817072353800c1820b21
mathkarl <mathkarl@LSE-B2725>
2020-06-04 11:25:47 +0100

    Add project files.

---
+++ ChatAppUDP2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ChatAppUDP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTaEmot = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbxMeddelande = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Serverns Portnummer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Inkorg";
+            // 
+            // btnTaEmot
+            // 
+            this.btnTaEmot.Location = new System.Drawing.Point(11, 214);
+            this.btnTaEmot.Name = "btnTaEmot";
+            this.btnTaEmot.Size = new System.Drawing.Size(304, 23);
+            this.btnTaEmot.TabIndex = 2;
+            this.btnTaEmot.Text = "Tag emot";
+            this.btnTaEmot.UseVisualStyleBackColor = true;
+            this.btnTaEmot.Click += new System.EventHandler(this.btnTaEmot_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // tbxMeddelande
+            // 
+            this.tbxMeddelande.Location = new System.Drawing.Point(11, 124);
+            this.tbxMeddelande.Multiline = true;
+            this.tbxMeddelande.Name = "tbxMeddelande";
+            this.tbxMeddelande.Size = new System.Drawing.Size(304, 84);
+            this.tbxMeddelande.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 249);
+            this.Controls.Add(this.tbxMeddelande);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTaEmot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTaEmot;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbxMeddelande;
+    }
+}
+
---
+++ ChatAppUDP2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatAppUDP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected void btnTaEmot_Click(object sender, EventArgs e)
+        {
+            IPEndPoint klientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            UdpClient client = new UdpClient(12345);
+            byte[] inström = client.Receive(ref klientEndPoint);
+            tbxMeddelande.Text = Encoding.Unicode.GetString(inström);
+        }
+    }
+}
